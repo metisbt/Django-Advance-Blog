86242a87afd55ee341392ff8536ee825cad22c01
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-22 05:05:24 +0330

    added user config to settings

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserManager(BaseUserManager):
@@ -39,13 +39,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     '''
     Custom User Model for our app
     '''
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255,unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verified = models.booleanField(default=False)
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
