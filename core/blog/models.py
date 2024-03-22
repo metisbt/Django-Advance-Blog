d4a48e7c1b624875004d306d947a7e7c1892ccae
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-22 04:17:37 +0330

    added blog model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    '''
+    this is a class to define posts for blog app
+    '''
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    image = models.Field(null=True, blank=True)
+    authot = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
