b0190d9a5b258f20951feb6e22aad36ab9ef21c9
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-25 00:25:05 +0330

    use RedirectView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from blog import views
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
+app_name = 'blog'
 
 urlpatterns = [
     path('fbv-index',views.indexView, name='fbv-test'),
     path("cbv-index", views.IndexView.as_view(), name='cbv-index'),
+    path(
+        "go-to-index/",
+        RedirectView.as_view(pattern_name="blog:index"),
+        name="redirect-to-index",
+    ),
 ]
