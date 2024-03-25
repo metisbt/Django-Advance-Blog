9a8f4e2353479e35f8468e892f8ba806b5418b9a
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-25 04:46:39 +0330

    migrate blog user to profile

--- core/blog/models.py
+++ core/blog/models.py
@@ -3,7 +3,7 @@ from accounts.models import User
 from django.contrib.auth import get_user_model
 
 # getting user model object
-User = get_user_model()
+# User = get_user_model()
 
 class Post(models.Model):
     '''
@@ -11,8 +11,8 @@ class Post(models.Model):
     '''
     title = models.CharField(max_length=250)
     content = models.TextField()
-    image = models.Field(null=True, blank=True)
-    authot = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
     status = models.BooleanField()
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 
