02b6a1c8eb12abdd8764a2b7d7f5d9b0411b506d
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-27 14:52:13 +0330

    PosetSerializer with ModelSerializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
+from blog.models import Post
 
+
+# with Serializer
+'''
 class PostSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
+'''
+
+# with ModelSerializer
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'status', 'content', 'created_date', 'published_date']
