426740440e263d8b5bc24c0ad6b7ecd15c9d03d0
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-27 18:10:45 +0330

    added permissions settings for rest_framework in core settings and api view

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import *
 from blog.models import Post
@@ -20,6 +21,7 @@ def postList(request):
 
 
 @api_view(["GET", "PUT", "DELETE"])
+@permission_classes([IsAuthenticated])
 def postDetail(request,id):
     # return 404
     '''
