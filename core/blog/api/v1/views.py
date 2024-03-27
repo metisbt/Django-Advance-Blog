f41203ffeb011437e097d31721bd0ffbcf25bca2
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-27 14:40:34 +0330

    404 solutions for response

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
 from blog.models import Post
+from django.shortcuts import get_object_or_404
+
 
 @api_view()
 def postList(request):
@@ -9,6 +11,18 @@ def postList(request):
 
 @api_view()
 def postDetail(request,id):
-    post = Post.objects.get(pk=id)
+    # return 404
+    '''
+    from rest_framework import status
+
+    try:
+        post = Post.objects.get(pk=id)
+        serialize = PostSerializer(post)
+        return Response(serialize.data)
+    except Post.DoesNotExist:
+        return Response({"detail":"Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    '''
+    # return 404 better one
+    post = get_object_or_404(Post,pk=id)
     serialize = PostSerializer(post)
     return Response(serialize.data)
