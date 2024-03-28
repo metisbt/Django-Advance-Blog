89b827e453953b04fd07c6bc14c4ddd4cd128c4d
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-28 20:45:48 +0330

    added paginations.py and PageNumberPagination

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,6 +10,7 @@ from rest_framework.generics import GenericAPIView, ListAPIView, ListCreateAPIVi
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from .paginations import DefaultPagination
 
 
 # function with APIView
@@ -183,6 +184,7 @@ class PostModelViewSet(viewsets.ModelViewSet):
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
     ordering_fields = ['published_date']
+    pagination_class = DefaultPagination
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
