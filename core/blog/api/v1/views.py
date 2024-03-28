721c35cee82a059b4d29c863986f530af44b315f
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-28 20:38:13 +0330

    added OrderingFilter to views api>v1

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 # function with APIView
@@ -179,9 +179,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
