c64a3c5d79c878432dd67d58c189c275db6f6c0d
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-03-28 19:19:00 +0330

    added SlugRelatedField for category

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -11,12 +11,22 @@ class PostSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 '''
 
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
 # with ModelSerializer
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     # for serializers methods that define here
     absolute_url = serializers.SerializerMethodField()
+    
+    # display name instead of id for category
+    # category = serializers.SlugRelatedField(many=False,slug_field='name', queryset=Category.objects.all())
+    category = CategorySerializer()
 
     class Meta:
         model = Post
@@ -25,9 +35,6 @@ class PostSerializer(serializers.ModelSerializer):
     def get_absolute_url(self,obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj)
-
-class CategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
+    
+    def to_representation(self, instance):
+        return super().to_representation(instance)
