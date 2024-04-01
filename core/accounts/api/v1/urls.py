2d0453ad3131bb8b9843e24a19554fe610878972
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-01 16:27:18 +0330

    added CustomObtainAuthToken to views and CustomAuthTokenSerializer to serializers in api>v1

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from rest_framework.authtoken import views
+# from rest_framework.authtoken import views
 
 
 app_name = 'ap-v1'
@@ -8,7 +8,7 @@ app_name = 'ap-v1'
 urlpatterns = [
     # registration
     path('registration/', views.RegistrationApiView.as_view(),name='registration'),
-    path('token/login', views.obtain_auth_token.as_view(),name='token-login'),
+    path('token/login/', views.CustomObtainAuthToken.as_view(),name='token-login'),
     # change password
     # reset password
     # login token
