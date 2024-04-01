795d21dba96c57c6f7c8595587a489390097b2d7
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-01 16:02:53 +0330

    added obtain_auth_token to api>v1 urls

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken import views
+
 
 app_name = 'ap-v1'
 
 urlpatterns = [
     # registration
-    path('registration/', views.RegistrationApiView.as_view(),name='registration')
+    path('registration/', views.RegistrationApiView.as_view(),name='registration'),
+    path('token/login', views.obtain_auth_token.as_view(),name='token-login'),
     # change password
     # reset password
     # login token
