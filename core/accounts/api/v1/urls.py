23b7533e398aba818d7ddbf6524e04615b651734
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-02 01:04:37 +0330

    added jwt but NOT USED

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from . import views
 # from rest_framework.authtoken import views
+# for JWT
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+#     TokenVerifyView,
+# )
 
 
 app_name = 'ap-v1'
@@ -14,5 +20,8 @@ urlpatterns = [
     path('token/login/', views.CustomObtainAuthToken.as_view(),name='token-login'),
     path('token/logout/', views.CustomDiscardAuthToken.as_view(),name='token-logout'),
     # login jwt
+    # path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create')
+    # path('jwt/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    # path('jwt/verify/', TokenVerifyView.as_view(), name='token-refresh'),
 ]
 
