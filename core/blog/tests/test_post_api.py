75ef46a3e493c60f93cb1275aef9acfe27ba894c
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-06 00:42:22 +0330

    added test_create_post_response_401_status

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,12 +1,25 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+from datetime import datetime
 
 # access pytest to my db
 @pytest.mark.django_db
 class TestPostApi:
+    client = APIClient()
+
     def test_get_post_response_200_status(self):
-        client = APIClient()
         url = reverse('blog:api-v1:post-list')
-        response = client.get(url)
+        response = self.client.get(url)
         assert response.status_code == 200
+
+    def test_create_post_response_401_status(self):
+        url = reverse('blog:api-v1:post-list')
+        data={
+            'title':'test with pytest',
+            'content':'description',
+            'status':True,
+            'published_date':datetime.now()
+        }
+        response = self.client.post(url,data)
+        assert response.status_code == 401
