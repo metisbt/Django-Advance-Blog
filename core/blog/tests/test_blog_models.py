1e4f19339a1953732c3cfce987944bdd9ddc68f2
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-05 16:54:05 +0330

    added setUp function for user and profile object

--- core/blog/tests/test_blog_models.py
+++ core/blog/tests/test_blog_models.py
@@ -6,20 +6,25 @@ from accounts.models import User, Profile
 
 class TestPostModel(TestCase):
 
-    def test_create_post_with_valid_data(self):
-        user = User.objects.create_user(email='ali@ali.com', password='a@/123456')
-        profile = Profile.objects.create(
-            user = user,
+    # for using user and profile object without create each time
+    def setUp(self):
+        self.user = User.objects.create_user(email='ali@ali.com', password='a@/123456')
+        self.profile = Profile.objects.create(
+            user = self.user,
             first_name = 'ali test',
             last_name = 'ali test',
             description = 'description test',
         )
+
+    def test_create_post_with_valid_data(self):
+        
         post = Post.objects.create(
             title = 'test for test model',
             content = 'description',
             status = True,
             category = None,
-            author = profile,
+            author = self.profile,
             published_date = datetime.now()
         )
-        self.assertEqual(post.title, 'test for test model')
+        # self.assertEqual(post.title, 'test for test model')
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
