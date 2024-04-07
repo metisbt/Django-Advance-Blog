e3b9eb713cec6e9421e978ae7b1fc9745135dca7
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-07 19:31:03 +0330

    Update insert_data.py
    + added category fake
    
    + added post fake

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -2,7 +2,15 @@ from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
 from accounts.models import User, Profile
 from blog.models import Post, Category
+import random
+from datetime import datetime
 
+
+category_list = [
+    'IT',
+    'Fun',
+    'Develop'
+]
 
 class Command(BaseCommand):
     help = "inserting dummy data"
@@ -12,9 +20,24 @@ class Command(BaseCommand):
         self.fake = Faker()
 
     def handle(self, *args, **options):
+        # create fake user and profile
         user = User.objects.create_user(email=self.fake.email(), password='Test@123456')
         profile = Profile.objects.get(user=user)
         profile.first_name = self.fake.first_name()
         profile.last_name = self.fake.last_name()
         profile.description = self.fake.paragraph(nb_sentences=5)
         profile.save()
+
+        # create fake category from my list
+        for name in category_list:
+            Category.objects.get_or_create(name=name)
+
+        for _ in range(10):
+            Post.objects.create(
+                author = profile,
+                title = self.fake.paragraph(nb_sentences=1),
+                content = self.fake.paragraph(nb_sentences=10),
+                status = random.choice([True,False]),
+                category = Category.objects.get(name = random.choice(category_list)),
+                published_date = datetime.now()
+            )
