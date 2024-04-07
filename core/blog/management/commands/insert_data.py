fc2c389f6a5ebb64dfd971be6402533e074beb91
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-07 19:20:13 +0330

    added Faker and insert_data.py and add fake user and profile

---
+++ core/blog/management/commands/insert_data.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+from accounts.models import User, Profile
+from blog.models import Post, Category
+
+
+class Command(BaseCommand):
+    help = "inserting dummy data"
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        user = User.objects.create_user(email=self.fake.email(), password='Test@123456')
+        profile = Profile.objects.get(user=user)
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.description = self.fake.paragraph(nb_sentences=5)
+        profile.save()
