2bb647c637faf7a69e48e0984759c0d077d47e68
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-14 23:53:59 +0330

    Update views.py
    + added cache_page decorator

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -4,13 +4,18 @@ import time
 from .tasks import sendEmail
 import requests
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
 
+@cache_page(60)
 def test(request):
-    if cache.get("test_delay_api") is None:
+    # without decorator
+    """if cache.get("test_delay_api") is None:
         response = requests.get('https://6b84698a-56f8-48db-88f3-cf1cf5a20948.mock.pstmn.io/test/delay/5')
-        cache.set("test_delay_api", response.json())
-    return JsonResponse(cache.get("test_delay_api"))
+        cache.set("test_delay_api", response.json())"""
+    # with decorator
+    response = requests.get('https://6b84698a-56f8-48db-88f3-cf1cf5a20948.mock.pstmn.io/test/delay/5')
+    return JsonResponse(response.json())
